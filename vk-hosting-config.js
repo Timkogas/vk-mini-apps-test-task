d9b54f1e105036c0d3d1de89fbd4900151f692f2
Timur Ermolaev <timkogas@gmail.com>
2023-03-16 17:33:46 +0300

    Небольшие изменения

--- vk-hosting-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  static_path: "build",
-  app_id: Number(process.env.REACT_APP_APP_ID),
-  endpoints: {
-    mobile: "index.html",
-    mvk: "index.html",
-    web: "index.html"
-  }
-}
